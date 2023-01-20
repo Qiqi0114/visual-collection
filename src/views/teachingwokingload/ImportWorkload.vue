7e03bf9b86870e23434c41709d394382d5503cca
七七 <1772014415@qq.com>
2023-01-20 21:32:02 +0800

    导入教学工作量70%,安装xlxs 添加工具包

--- src/views/teachingwokingload/ImportWorkload.vue
+++ src/views/teachingwokingload/ImportWorkload.vue
@@ -1,99 +1,140 @@
 <template>
-    <router-view />
-    <div class="mlist-bg" v-if="router.currentRoute.value.meta.show">
-        <!--页头-->
-        <Header />
-        <!--中间内容区域-->
-        <div class="container-bg">
-            <div class="container">
-                <!-- 搜索部分 -->
-                <div class="search-section">
-                    <el-row>
-                        <el-col>
-                            <el-form ref="searchFormRef" :model="searchForm" :inline="true" style="width:100%">
-                                <el-row>
-                                    <el-col :span="6">
-                                        <el-form-item label="合同编号" prop="contactNum">
-                                            <el-input v-model.contactNum="searchForm.contactNum"
-                                                placeholder="请输入合同编号" />
-                                        </el-form-item>
-                                    </el-col>
-                                    <el-col :span="6">
-                                        <el-form-item label="客户全称" prop="customerName">
-                                            <el-input v-model.customerName="searchForm.customerName"
-                                                placeholder="请输入客户全称" />
-                                        </el-form-item>
-                                    </el-col>
-                                    <el-col :span="6">
-                                        <el-form-item label="量价互保" prop="qphbFLAG">
-                                            <el-checkbox v-model.qphbFLAG="searchForm.qphbFLAG"
-                                                @change="selectForm(searchFormRef)" true-label="1" false-label="0">
-                                            </el-checkbox>
-                                        </el-form-item>
-                                    </el-col>
-                                    <el-col :span="24">
-                                        <el-form-item style="float: right;">
-                                            <el-button type="success" @click="selectForm(searchFormRef)">查询</el-button>
-                                            <el-button @click="resetForm()" type="info">重置</el-button>
-                                            <el-button type="primary" @click="addAgreementPrice()">新建格式合同
-                                            </el-button>
-                                            <el-button type="primary" @click="addFormatRentShip()">新建非格式合同
-                                            </el-button>
-                                        </el-form-item>
-                                    </el-col>
-                                </el-row>
-
-                            </el-form>
-                        </el-col>
-                    </el-row>
-                </div>
-
-                <!-- 表格部分 -->
-                <div class="table-bg" ref="tableContainer" style="width: 99%;">
-                    <el-table :data="baseInfoTableData" :border="true" @cell-click="clickAgreementPriceRow"
-                        ref="baseInfoTableDataRef" v-loading="loading" :header-cell-style="{ background: '#F5F6FA' }"
-                        :height="tableHeight">
-                        <el-table-column prop="contactNum" label="合同编号" min-width="130" />
-                        <el-table-column prop="portOffice" label="办事处" min-width="120" />
-                        <el-table-column prop="areaPart" label="片区" min-width="120" />
-                        <el-table-column prop="customerName" label="客户全称" min-width="260" />
-                        <el-table-column prop="registeredCapital" label="注册资金(万)" min-width="120" />
-                        <el-table-column prop="monthlyShipments" label="月发货量" min-width="120" />
-                        <el-table-column prop="monthlyShipmentsUnit" label="月发货量单位" min-width="120"/>
-                        <el-table-column prop="performPerson" label="谈判人" min-width="120" />
-                        <el-table-column prop="startDate" label="合同有效期起" min-width="130" />
-                        <el-table-column prop="endDate" label="合同有效期止 " min-width="130" />
-                        <el-table-column prop="flow" label="流向 " min-width="150" />
-                        <el-table-column prop="cargoType" label="货种" min-width="120" />
-                        <el-table-column prop="qphbFLAG" label="量价互保" min-width="85"/>
-                        <el-table-column prop="startFlowFlag" label="流程启动标记" min-width="140"/>
-                    </el-table>
-                </div>
-                <!--分页器 start-->
-<!--                 <div class="flex pagination-bg">
-                    <el-pagination v-model:currentPage="pCurrentPage" v-model:page-size="pPageSize"
-                        :page-sizes="[10, 20]" :small="pSmall" :disabled="pDisabled" :background="pBackground"
-                        layout="total, sizes, prev, pager, next, jumper" :total="pTotal" @size-change="handleSizeChange"
-                        @current-change="handleCurrentChange" />
-                </div> -->
-                <!--分页器 end-->
-
+    <div class="container-bg">
+        <div class="container">
+                <!-- 上传文件按钮 -->
+            <div class="buttonBox">
+            <el-upload
+                action
+                accept=".xlsx,.xls"
+                :auto-upload="false"
+                :show-file-list="false"
+                :on-change="handle"
+            >
+                <el-button type="primary" slot="trigger">选取 Excel 文件</el-button>
+            </el-upload>
+            <el-button type="success" @click="submit()">采集数据提交</el-button>
             </div>
 
+            <!-- 解析出来的数据 -->
+            <div class="tableBox" v-show="show">
+            <h3>
+                <i class="el-icon-info">
+                小主，以下是采集完成的数据，请您检查无误后，点击“采集数据提交”按钮上传至服务器</i
+                >
+            </h3>
+            <el-table     v-loading="loading"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)" :data="tempData" border style="width: 100%" :height="height">
+                <el-table-column
+                prop="name"
+                label="姓名"
+                min-width="50%"
+                ></el-table-column>
+                <el-table-column
+                prop="phone"
+                label="电话"
+                min-width="50%"
+                ></el-table-column>
+            </el-table>
+            </div>
         </div>
-        <!--页脚-->
-        <Footer />
-
-    </div>
+        </div>
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from "vue-router";
+import xlsx from 'xlsx'
+import { readFile, character, delay } from '../../assets/js/utils'
+import { ref } from "vue-demi"
+import { ElMessage } from 'element-plus';
+//加载
+const loading = ref<boolean>(false);
+//展示出解析出来的数据
+const show = ref<boolean>(false);let tempData = ref<any>([]);
 
-const router = useRouter();
+    // 采集 EXCEL 数据
+const handle = async(e:any) => {
+      let file = e.raw
+      if (!file) return
 
+      show.value = false
+      loading.value = true;
+
+      await delay(300)
+
+      // 读取FILE中的数据
+      let data = await readFile(file)
+      let workbook = xlsx.read(data, { type: 'binary' }),
+        worksheet = workbook.Sheets[workbook.SheetNames[0]],
+        list = xlsx.utils.sheet_to_json(worksheet)
+      // console.log(list)
+
+      // 把读取出来的数据变为可以提交为服务器的数据格式
+      let arr :any[]= []
+      let oldData = JSON.parse(window.localStorage.getItem('excel') || '[]')
+      let index = oldData.length
+      list.forEach(item => {
+        let obj = {}
+        for (let key in character) {
+          if (!character.hasOwnProperty(key)) break
+          let v = character[key],
+            text = v.text,
+            type = v.type
+          v = item[text] || ''
+          type === 'string' ? (v = String(v)) : null
+          type === 'number' ? (v = Number(v)) : null
+          obj[key] = v
+        }
+        obj.id = ++index
+        obj.time = new Date()
+        arr.push(obj)
+      })
+
+      await delay(300)
+
+      // 展示到页面中
+      show.value = true
+      // console.log(arr)
+      tempData = arr
+      loading.value = false;
+    }
+
+    // 提交数据给服务器
+    const submit =() =>{
+      if (tempData.length <= 0) {
+        return ElMessage.warning({
+          message: '小主，请您先选择 EXCEL 文件！',
+          type: 'warning',
+          showClose: true
+        })
+      }
+      let oldData = JSON.parse(window.localStorage.getItem('excel') || '[]'),
+        newData = [...oldData, ...tempData]
+      window.localStorage.setItem('excel', JSON.stringify(newData))
+      
+    }
+  
 </script>
 
 <style lang="scss" scoped>
-@import "../../style/public.scss"
+@import "../../style/public.scss";
+
+.buttonBox {
+  padding: 15px;
+  display: flex;
+  width: 35%;
+  justify-content: flex-start;
+  & .el-button {
+    margin-right: 20px !important;
+  }
+}
+
+.tableBox {
+  padding: 0 15px;
+  h3 {
+    font-size: 18px;
+    color: #f56c6c;
+    padding-bottom: 15px;
+  }
+}
 </style>
