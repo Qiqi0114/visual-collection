3464f7b2d488db95caa2895c2fd2e02d6103aa53
七七 <1772014415@qq.com>
2023-02-25 02:19:12 +0800

    修改柱状图,接口获取数据对二维对象\数组算法存储二维数组,添加获取总系教学工作量数据图,

--- src/views/visualizationAttempts/visualizationAttempts_1.vue
+++ src/views/visualizationAttempts/visualizationAttempts_1.vue
@@ -44,26 +44,69 @@
             </el-form>
           </el-col>
         </el-row>
-      </div>
-
-  <div id="box" style="width: 100%; height: 400px">  <el-empty description="description" /></div>
-
+    </div>
+  <div id="box" style="width: 100%; height: 40%">  <el-empty description="description" /></div>
+      <!-- 总系搜索部分 -->
+      <div class="search-section">
+        <el-row>
+          <el-col>
+            <el-form
+              ref="searchZFormRef"
+              :model="searchZForm"
+              :inline="true"
+              style="width: 100%"
+            >
+              <el-row>
+                <el-col :span="4">
+                  <el-form-item label="年限" prop="yearId">
+                    <el-select v-model="searchZForm.yearId" filterable placeholder="请选择"  clearable>
+                        <el-option
+                          v-for="item in  YearList.YearListCode"
+                          :key="item.value"
+                          :label="item.label"
+                          :value="item.value"
+                        >
+                        </el-option>
+                      </el-select>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="6">
+                  <el-form-item style="float: right">
+                    <el-button type="primary" @click="selectZForm(searchZFormRef)"
+                      >查询</el-button
+                    >
+                    <el-button @click="resetZForm()" type="info">重置</el-button>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+            </el-form>
+          </el-col>
+        </el-row>
+    </div>
+  <div id="boxZ" style="width: 100%; height: 40%">  <el-empty description="description" /></div>
 </template>
 
 <script lang="ts" setup>
 import { ElMessage, FormInstance } from 'element-plus';
+import { object } from 'prop-types';
 import { onMounted, inject, reactive, ref } from 'vue'; 
 import { DepartmentListAPI } from '../../api/accountManagement';
 import { getYearListAPI } from '../../api/teachingwokingload';
 import { getYearByZAPI, workLoadListAPI } from "../../api/visualizationAttempts";
 const searchFormRef = ref<FormInstance>()
-//查询参数
+//查询系z参数
 const searchForm = reactive({
      departmentId:"1",
      yearId:"1",
 })
-let departmentList = reactive({departmentListCode:[] as any})
+const searchZFormRef = ref<FormInstance>()
+//查询总系参数
+const searchZForm = reactive({
+     yearId:"1",
+})
 
+//系选中
+let departmentList = reactive({departmentListCode:[] as any})
 //获取系列表
 const getDepartmentList = async() => {
     try{
@@ -82,9 +125,9 @@ const getDepartmentList = async() => {
         console.log(e,'e');
     }
 }
+
 //年选择
 let YearList = reactive({YearListCode:[] as any})
-
 //获取年限列表
 const getYearList = async() => {
     try{
@@ -109,6 +152,7 @@ const resetForm = () => {
     //清空查询框数据
     searchForm.departmentId = "";
     searchForm.yearId = "";
+    
     //重新获取数据
     workLoadList();
 };
@@ -124,35 +168,47 @@ const selectForm = (formEl: FormInstance | undefined) => {
           }
      })
 }
+// 重置总系查询条件
+const resetZForm = () => {
+    //清空查询框数据
+    searchZForm.yearId = "";
+    
+    //重新获取数据
+    getYearByZ();
+};
+//查询总系分页列表
+const selectZForm = (formEl: FormInstance | undefined) => {
+     if(!formEl) return;
+     formEl.validate((valid) => {
+          if(valid){
+            getYearByZ();
+          }else{
+               console.log('error');
+               return false;
+          }
+     })
+}
     type EChartsOption = echarts.EChartsOption;
     var option: EChartsOption;
     let echarts: any = inject('echarts');
     const change = () => {
         const myChart = echarts.init(document.getElementById('box'));
         option = {
-/*             xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            },
-            yAxis: {
-                type: 'value',
-            },
-            series: [
-                {
-                    data: [150, 230, 224, 218, 135, 147, 260],
-                    type: 'line',
-                },
-            ], */
-            xAxis: {
-                data: userName
-            },
-            yAxis: {},
-            series: [
-                {
-                type: 'bar',
-                data: userDataZ
-                }
-            ]
+          legend: {},
+          dataset: {
+            source: sourceData
+          },
+          // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。将x轴标签全显示出来axisLabel
+          xAxis: { 
+            type: 'category',
+            axisLabel:{
+              interval:0,
+            }
+          },
+          // 声明一个 Y 轴，数值轴。
+          yAxis: {},
+          // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+          series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
         };
         option && myChart.setOption(option);
         // 根据页面大小自动响应图表大小
@@ -160,23 +216,52 @@ const selectForm = (formEl: FormInstance | undefined) => {
             myChart.resize();
         });
     };
-//人员
-let userName :Array<string>= new Array<string>()
-//人员z数据
-let userDataZ :Array<string>= new Array<string>()
+    const changeZ = () => {
+        const myChart = echarts.init(document.getElementById('boxZ'));
+        option = {
+          legend: {},
+          dataset: {
+            source: zData
+          },
+          // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。将x轴标签全显示出来axisLabel
+          xAxis: { 
+            type: 'category',
+            axisLabel:{
+              interval:0,
+            }
+          },
+          // 声明一个 Y 轴，数值轴。
+          yAxis: {},
+          // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+          series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+        };
+        option && myChart.setOption(option);
+        // 根据页面大小自动响应图表大小
+        window.addEventListener('resize', function () {
+            myChart.resize();
+        });
+    };
+//数据
+let sourceData :Array<Array<string>> = new Array<Array<string>>();
 //获取教学工作量
 const workLoadList = async() =>{
     try{
-        userName = [];
-        userDataZ = [];
+        sourceData = [];
         const res = await workLoadListAPI({
             yearId:searchForm.yearId,
             mentId:searchForm.departmentId,
         })
         if(res.data.code === 200){
-            res.data.data.forEach((item: { userName: string; z: string; }) => {
-                userName.push(item.userName)
-                userDataZ.push(item.z)
+          let temp :string[] = [];
+          temp.push('product', 's1合计', '教学业绩分值F', '未乘就业率系数的总工作量Z')
+          sourceData.push(temp);
+            res.data.data.forEach((item: { userName: string; z: string; s1: string; f: string; }) => {
+                let temp :string[] = [];
+                temp.push(item.userName)
+                temp.push(item.s1)
+                temp.push(item.f)
+                temp.push(item.z)
+                sourceData.push(temp);
             });
             change();
         }else{
@@ -186,14 +271,32 @@ const workLoadList = async() =>{
 
     }
 }
+//总系数据
+let zData :Array<Array<string>> = new Array<Array<string>>();
 //获取教学工作量合计
 const getYearByZ = async() =>{
     try{
+        zData = [];
         const res = await getYearByZAPI({
-            yearId:'',
+            yearId:searchZForm.yearId,
         })
+        let temp :string[] = [];
+        temp.push('product', 's1合计', '教学业绩分值F', '未乘就业率系数的总工作量Z')
+        zData.push(temp);
         if(res.data.code === 200){
-
+          let name :string[] = Object.keys(res.data.data)
+          for(let key in res.data.data){
+            let temp :string[] = [];
+            temp.push(res.data.data[key].s1)
+            temp.push(res.data.data[key].f)
+            temp.push(res.data.data[key].z)
+            zData.push(temp);
+          }
+          for(let key = 1; key < zData.length; key++ ){
+            zData[key].unshift(name[key - 1],0)
+          }
+          console.log(zData);
+          changeZ();
         }else{
             
         }
@@ -207,6 +310,7 @@ onMounted(()=>{
     //获取年限列表
     getYearList();
     workLoadList();
+    getYearByZ();
 })
 </script>
 
